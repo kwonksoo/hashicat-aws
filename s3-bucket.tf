06fcd7f630d7fab01102be7e5e73b1e7fb186249
Kyungsoo Kwon <kwonksoo91@gmail.com>
2024-12-03 07:42:41 +0000

    Added s3 bucket module(changed bucket name)

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/kwonksoo/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "kyungsoo_kwon"
+  bucket = "kyungsoo_kwon_test_bucket6345234"
 }
